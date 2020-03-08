b1640b4c088b65218d86159175ae7c0a41fd9489
andreas@andreas-hahn.at <andreas@andreas-hahn.at>
2020-03-08 14:01:00 +0100

    updated styling

--- src/OrdinaryComponent.js
+++ src/OrdinaryComponent.js
@@ -1,10 +1,16 @@
 export class OrdinaryComponent extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({mode: "open"}).innerHTML = this.template;
+        this.attachShadow({mode: "closed"}).innerHTML = this.template;
     }
 
     get template() {
-        return `<div class="ordinary-text">Ordinary</div>`;
+        return `
+        <style>
+            div{
+            color: #daa520;
+            }
+        </style>
+        <div class="ordinary-text">Ordinary</div>`;
     }
 }
